6c27baf245fb898ac1819f4198383ac9e35b754e
macbook <macbook@MacBook-Air.local>
2025-11-30 14:07:55 +0100

    Deployment issues 4

--- app/oauth/layout.tsx
+++ app/oauth/layout.tsx
@@ -1,6 +1,7 @@
 import ProtectedPublicRoute from "@/providers/ProtectedPublicRoute";
 import React from "react";
 
+
 const layout = ({ children }: {
     children: React.ReactNode
 }): React.ReactElement => {
